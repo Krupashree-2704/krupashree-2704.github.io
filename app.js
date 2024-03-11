af29023410a71df82e0a5b51fa816481bf22ba67
Krupashree-2704 <80285220+Krupashree-2704@users.noreply.github.com>
2024-03-11 21:35:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,93 @@
+function fn(user_click) {
+    if(user_click=='Bullet')
+    {
+        document.getElementById('motorcycleImg').src="./images/img_js/bullet350.avif" ;
+    }
+    else if (user_click=='Interceptor') {
+        document.getElementById('motorcycleImg').src="./images/img_js/interceptor.jpg" ;
+    }
+    else if (user_click=='Continental') {
+        document.getElementById('motorcycleImg').src="./images/img_js/continental gt.jpg" ;
+    }
+    else if (user_click=='SuperMeteor') {
+        document.getElementById('motorcycleImg').src="./images/img_js/super meteor 650.jpg" ;
+    }
+    else if (user_click=='Hunter350') {
+        document.getElementById('motorcycleImg').src="./images/img_js/hunter 350.jpg" ;
+    }
+    else if (user_click=='Scram') {
+        document.getElementById('motorcycleImg').src="images/img_js/scram 411.jpg" ;
+    }
+    else if (user_click=='Classic') {
+        document.getElementById('motorcycleImg').src="./images/img_js/classic.jpeg" ;
+    }
+    else if (user_click=='Meteor') {
+        document.getElementById('motorcycleImg').src="./images/img_js/merteore.jpeg" ;
+    }
+    else if (user_click=='Himalayan') {
+        document.getElementById('motorcycleImg').src="./images/img_js/himalayan.avif" ;
+    }
+}
+function fn1(user_choice) {
+    if(user_choice=='Trackschool')
+    {
+        document.getElementById('rideImg').src="./images/ridesection_img/track school.avif" ;
+    }
+    else if (user_choice=='GTCup') {
+        document.getElementById('rideImg').src="./images/ridesection_img/gt cup.jpeg" ;
+    }
+    else if (user_choice=='Rentals') {
+        document.getElementById('rideImg').src="./images/ridesection_img/rental.avif" ;
+    }
+    else if (user_choice=='REMapped') {
+        document.getElementById('rideImg').src="./images/ridesection_img/remapped.jpg" ;
+    }
+    else if (user_choice=='LehRiderSupport') {
+        document.getElementById('rideImg').src="./images/ridesection_img/leh-rider-support.jpg" ;
+    }
+    else if (user_choice=='GoaGarageCafe') {
+        document.getElementById('rideImg').src="./images/ridesection_img/goa-garage-cafe-new.jpg" ;
+    }
+    else if (user_choice=='Motorverse') {
+        document.getElementById('rideImg').src="./images/ridesection_img/motoverse-2024.jpeg" ;
+    }
+    // else if (user_click=='Himalayan') {
+    //     document.getElementById('motorcycleImg').src="./images/img_js/himalayan.avif" ;
+    // }
+}
+function fn2(user_choose) {
+    if(user_choose=='TCX')
+    {
+        document.getElementById('apparelDiv').src= "./images/apparelsection_img/royal-enfield-tcx.jpg";
+    }
+    else if (user_choose=='MIY') {
+        document.getElementById('apparelDiv').src= "./images/apparelsection_img/miy-riding-jacket.avif";
+    }
+    else if (user_choose=='Alpenista') {
+        document.getElementById('apparelDiv').src="./images/apparelsection_img/royal-enfield-x-alpinestar.jpg";
+    }
+}
+function fn3(user_ch) {
+    if(user_ch=='MakeIY')
+    {
+        document.getElementById('accesoriesDiv').src="./images/accesories_img/make it yours.jpg" ;
+    }
+    else if (user_ch=='Geninue') {
+        document.getElementById('accesoriesDiv').src="./images/accesories_img/geninuemotorcycleaccesories.avif" ;
+    }
+    // else if (user_choose=='Alpenista') {
+    //     document.getElementById('apparelDiv').src="./images/apparelsection_img/royal-enfield-tcx.jpg";
+    // }
+}
+function fn4(users_choice) {
+    if(users_choice=='Service')
+    {
+        document.getElementById('locateDiv').src="./images/locateus_img/service-centres.jpg" ;
+    }
+    else if (users_choice=='GeninueParts') {
+        document.getElementById('locateDiv').src="./images/locateus_img/genuine-parts.avif" ;
+    }
+    else if (users_choice=='Stores') {
+        document.getElementById('locateDiv').src="./images/locateus_img/stores.jpg";
+    }
+}
